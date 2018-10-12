641b61961bd58ecaa065e0d9c475a0029161a84d
yskimsun <yskimsun@naonsoft.com>
2018-10-12 16:10:44 +0900

    electron update success version!!

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 ﻿const { app, BrowserWindow } = require('electron')
+const log = require('electron-log');
+const {autoUpdater} = require("electron-updater");
 
-  const isDevelopment = process.env.NODE_ENV === 'dev'
+const isDevelopment = process.env.NODE_ENV === 'dev'
+
+
+
+autoUpdater.logger = log;
+autoUpdater.logger.transports.file.level = 'info';
+log.info('App starting...');
 
   // Keep a global reference of the window object, if you don't, the window will
   // be closed automatically when the JavaScript object is garbage collected.
@@ -19,7 +27,12 @@
         win.webContents.openDevTools()
     }
 
-  
+    win.webContents.on('did-finish-load', () => {
+        autoUpdater.checkForUpdatesAndNotify();
+        sendStatusToWindow('Checking for update...');
+    })
+
+
     // Emitted when the window is closed.
     win.on('closed', () => {
       // Dereference the window object, usually you would store windows
@@ -53,3 +66,35 @@
   
   // In this file you can include the rest of your app's specific main process
   // code. You can also put them in separate files and require them here.
+
+//const { ipcRenderer } = require('electron');
+
+  function sendStatusToWindow(text) {
+    log.info(text);
+    //win.webContents.send('messages', text);
+    win.webContents.send('update-msg', text)
+
+  }
+
+  autoUpdater.on('checking-for-update', () => {
+    sendStatusToWindow('Checking for update...');
+  })
+  autoUpdater.on('update-available', (info) => {
+    sendStatusToWindow('Update available.');
+  })
+  autoUpdater.on('update-not-available', (info) => {
+    sendStatusToWindow('Update not available.');
+  })
+  autoUpdater.on('error', (err) => {
+    sendStatusToWindow('Error in auto-updater. ' + err);
+  })
+  autoUpdater.on('download-progress', (progressObj) => {
+    let log_message = "Download speed: " + progressObj.bytesPerSecond;
+    log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+    log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+    sendStatusToWindow(log_message);
+  })
+  autoUpdater.on('update-downloaded', (info) => {
+    sendStatusToWindow('Update downloaded');
+  });
+
